c3ba1f2bd7e0d92dbf637a5c562f58177d6d3c2c
yangyushu <yyshu@hisw.cn>
2022-09-13 17:51:08 +0800

    学习部分组件

---
+++ App.tsx
@@ -0,0 +1,13 @@
+// In App.js in a new project
+
+import * as React from 'react';
+// import Index from './src/components/ActivityIndicator'; // 加载
+// import Index from './src/components/Button'; // 按钮
+// import Index from './src/components/FlatList'; // 列表
+import Index from './src/components/Image'; // 图片
+
+function App() {
+  return <Index />;
+}
+
+export default App;
